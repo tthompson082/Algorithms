4d4255a594be24cfdc89b5bd3ab1e8c5cc7dbafe
Tyler Thompson <tthompson082@gmail.com>
2020-02-20 18:16:30 -0500

    Add solution for rps

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python
+# can use pennies, nickels, dimes, quarters, and half-dollars: [1, 5, 10, 25, 50]
+# For 0 there are 0 ways
+# For 1: 1 penny
+# For 2: 1 penny 1 penny
+# For 3: 1 penny 1 penny 1 penny
+# for 4: 1 penny 1 penny 1 penny 1 penny
+# for 5: 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 5: 1 nickel
+# for 6: 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 6: 1 nickel 1 penny
+# for 7: 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 7: 1 nickel 1 penny 1 penny
+# for 8: 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 8: 1 nickel 1 penny 1 penny 1 penny
+# for 9: 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 9: 1 nickel 1 penny 1 penny 1 penny 1 penny
+# for 10: 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 10: 1 nickel 1 penny 1 penny 1 penny 1 penny 1 penny
+# for 10: 1 nickel 1 nickel
+# for 10: 1 dime
 
 import sys
 
+
 def making_change(amount, denominations):
-  pass 
+    pass
 
 
 if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python
+# for 1: [['rock'], ['paper'], ['scissors']]
+# for 2: [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+# for 3: [[r,r,r], [r,r,p], [r,r,s], [r,p,r], [r,p,p], [r,p,s], [r,s,r], [r,s,p], [r,s,s], [p,r,r], [p,r,p], [p,r,s], [p,p,r], [p,p,p], [p,p,s], [p,s,r], [p,s,p], [p,s,s], [s,r,r], [s,r,p], [s,r,s], [s,p,r], [s,p,p], [s,p,s], [s,s,r], [s,s,p], [s,s,s]]
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    rps_plays = ['rock', 'paper', 'scissors']
+    possible_combinations = []
+    if n < 0:
+        return 0
+    elif n == 0:
+        return [[]]
+    else:
+        def inner_function(play):
+            if len(play) == n:
+                possible_combinations.append(play)
+            else:
+                for i in range(len(rps_plays)):
+                    value = play + [rps_plays[i]]
+                    inner_function(value)
+        inner_function([])
+    return possible_combinations
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
